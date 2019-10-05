bb0c9fd27471e8f30fa4775d1b66657be65c8cb7
Udit Pandey <udit.kumar157@gmail.com>
2019-10-05 20:30:40 +0530

    Added changes

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class purchase(models.Model): #name of table
+
+   # id = models.IntegerField(blank=False)
+    name = models.CharField(max_length=50, blank=False)
+    phone = models.IntegerField()
+    amount = models.IntegerField()
+
+    def __str__(self):
+        return 'Id : {0} Name : {1}',format(self.id, self.name)
