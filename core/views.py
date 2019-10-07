91da14a0ad488e6b9a186a1406359fcfd1c4af47
Udit Pandey <udit.kumar157@gmail.com>
2019-10-08 02:50:50 +0530

    Added form and connected to database

--- core/views.py
+++ core/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Purchase
+from .forms import PurchaseForm
+from django.conf import settings
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib import messages
 # Create your views here.
 
-def index(request):
+def base(request):
     orders = Purchase.objects.all()
-    return render(request, 'index.html', {'orders': orders})
+    return render(request, 'base.html', {'orders': orders})
+
+def index(request):
+    if request.POST:
+        form = PurchaseForm(request.POST)
+        if form.is_valid():
+            if form.save():
+                return redirect('index/', messages.success(request, 'Order was successfully created.', 'alert-success'))
+            else:
+                return redirect('index/', messages.error(request, 'Data is not saved', 'alert-danger'))
+        else:
+            return redirect('index/', messages.error(request, 'Form is not valid', 'alert-danger'))
+    else:
+        form = PurchaseForm()
+        return render(request, 'index.html', {'form':form})
